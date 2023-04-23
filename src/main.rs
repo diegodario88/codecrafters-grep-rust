f78a12786044e3e185e890814f8b8cb759c92cd2
Diego Dario <diego.dario@gazin.com.br>
2023-04-23 16:35:34 -0300

    pass match digits

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,15 @@ use std::env;
 use std::io;
 use std::process;
 
-fn match_pattern(input_line: &str, pattern: &str) -> bool {
-    if pattern.chars().count() == 1 {
-        return input_line.contains(pattern);
-    } else {
-        panic!("Unhandled pattern: {}", pattern)
+enum Pattern {
+    SingleChar(char),
+    Digit,
+}
+
+fn match_pattern(input_line: &str, pattern: Pattern) -> bool {
+    match pattern {
+        Pattern::SingleChar(c) => input_line.contains(c),
+        Pattern::Digit => input_line.chars().any(|c| c.is_numeric()),
     }
 }
 
@@ -16,12 +20,23 @@ fn main() {
         process::exit(1);
     }
 
-    let pattern = env::args().nth(2).unwrap();
+    let pattern_arg = env::args().nth(2).unwrap();
+    let pattern = match pattern_arg.as_str() {
+        r"\d" => Pattern::Digit,
+        s => {
+            if s.chars().count() == 1 {
+                Pattern::SingleChar(s.chars().next().unwrap())
+            } else {
+                panic!("Unhandled pattern: {}", s)
+            }
+        }
+    };
+
     let mut input_line = String::new();
 
     io::stdin().read_line(&mut input_line).unwrap();
 
-    if match_pattern(&input_line, &pattern) {
+    if match_pattern(&input_line, pattern) {
         process::exit(0)
     } else {
         process::exit(1)
