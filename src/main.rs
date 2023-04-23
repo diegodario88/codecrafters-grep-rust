2dd2fd78240c7a5406055b1422cdcfd10a464288
Diego Dario <diego.dario@gazin.com.br>
2023-04-23 16:59:32 -0300

    pass match alphanumerics

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,18 @@ use std::env;
 use std::io;
 use std::process;
 
+#[derive(Debug)]
 enum Pattern {
     SingleChar(char),
     Digit,
+    Alphanumeric,
 }
 
 fn match_pattern(input_line: &str, pattern: Pattern) -> bool {
     match pattern {
         Pattern::SingleChar(c) => input_line.contains(c),
         Pattern::Digit => input_line.chars().any(|c| c.is_numeric()),
+        Pattern::Alphanumeric => input_line.chars().all(|c| c.is_alphanumeric()),
     }
 }
 
@@ -23,6 +26,7 @@ fn main() {
     let pattern_arg = env::args().nth(2).unwrap();
     let pattern = match pattern_arg.as_str() {
         r"\d" => Pattern::Digit,
+        r"\w" => Pattern::Alphanumeric,
         s => {
             if s.chars().count() == 1 {
                 Pattern::SingleChar(s.chars().next().unwrap())
@@ -36,7 +40,7 @@ fn main() {
 
     io::stdin().read_line(&mut input_line).unwrap();
 
-    if match_pattern(&input_line, pattern) {
+    if match_pattern(&input_line.trim(), pattern) {
         process::exit(0)
     } else {
         process::exit(1)
