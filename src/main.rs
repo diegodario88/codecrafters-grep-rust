1be926b6ff2740ce5a5ba39b73d5671a46fe15fe
Diego Dario <diego.dario@gazin.com.br>
2023-04-24 08:35:17 -0300

    daily

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ enum Pattern {
     SingleChar(char),
     Digit,
     Alphanumeric,
+    Group(String),
 }
 
 fn match_pattern(input_line: &str, pattern: Pattern) -> bool {
@@ -14,6 +15,23 @@ fn match_pattern(input_line: &str, pattern: Pattern) -> bool {
         Pattern::SingleChar(c) => input_line.contains(c),
         Pattern::Digit => input_line.chars().any(|c| c.is_numeric()),
         Pattern::Alphanumeric => input_line.chars().all(|c| c.is_alphanumeric()),
+        Pattern::Group(s) => {
+            let group_without_brackets = s.trim_start_matches('[').trim_end_matches(']');
+
+            // Handle negative character groups (denoted by a leading '^' character).
+            // If the group is negative, then return true only if the input_line does not contain any of the characters in the group.
+            if group_without_brackets.starts_with('^') {
+                group_without_brackets
+                    .chars()
+                    .all(|c| !input_line.contains(c))
+            }
+            // Otherwise, return true only if the input_line contains at least one character in the group.
+            else {
+                group_without_brackets
+                    .chars()
+                    .any(|c| input_line.contains(c))
+            }
+        }
     }
 }
 
@@ -27,13 +45,9 @@ fn main() {
     let pattern = match pattern_arg.as_str() {
         r"\d" => Pattern::Digit,
         r"\w" => Pattern::Alphanumeric,
-        s => {
-            if s.chars().count() == 1 {
-                Pattern::SingleChar(s.chars().next().unwrap())
-            } else {
-                panic!("Unhandled pattern: {}", s)
-            }
-        }
+        s if s.chars().count() == 1 => Pattern::SingleChar(s.chars().next().unwrap()),
+        s if s.starts_with("[") && s.ends_with("]") => Pattern::Group(String::from(s)),
+        s => panic!("Unhandled pattern: {}", s),
     };
 
     let mut input_line = String::new();
@@ -44,5 +58,28 @@ fn main() {
         process::exit(0)
     } else {
         process::exit(1)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_match_pattern() {
+        assert!(match_pattern("apple", Pattern::SingleChar('a')));
+        assert!(!match_pattern("apple", Pattern::SingleChar('b')));
+
+        assert!(match_pattern("123", Pattern::Digit));
+        assert!(!match_pattern("abc", Pattern::Digit));
+
+        assert!(match_pattern("apple123", Pattern::Alphanumeric));
+        assert!(!match_pattern("apple.123", Pattern::Alphanumeric));
+
+        assert!(match_pattern("apple", Pattern::Group(String::from("abc"))));
+        assert!(!match_pattern("dog", Pattern::Group(String::from("abc"))));
+
+        assert!(match_pattern("dog", Pattern::Group(String::from("^abc"))));
+        assert!(!match_pattern("cab", Pattern::Group(String::from("^abc"))));
     }
 }
